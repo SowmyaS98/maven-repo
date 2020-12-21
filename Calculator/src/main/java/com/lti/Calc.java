c92e992c2f59f53847c16687dc1a768a22511db1
SowmyaS98 <sowmyselvaraj.ssk@gmail.com>
2020-12-21 14:58:17 +0530

    added sub method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -5,10 +5,17 @@ public class Calc {
 	{
 		return n1+n2;
 	}
+	
+	public static  int sub(int n1,int n2)
+	{
+		return n1-n2;
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(add(10,20));
+		System.out.println("sum is: "+add(10,20));
+		System.out.println("diff is: "+sub(30,20));
 
 	}
 
